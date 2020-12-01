d65006774bba43179a011a1b0970431ef47c5595
cdubles <33189090+cdubles@users.noreply.github.com>
2020-12-01 08:27:57 -0500

    Show fake-data

--- client.js
+++ client.js
@@ -1,1 +1,71 @@
-document.querySelector("p").style.color = "red"
+
+console.log(loadData())
+
+
+function loadData() {
+    //JSON - JavaScript Object Notation
+    const movies = [
+    
+        
+    {
+        //key : value
+        "name" : "The Kingsman",
+        "genre" : "Action",
+        "reviews" : [
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Jacob Schafer",
+                    "gender" : "male",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Cody Schafer",
+                    "gender" : "male",
+                    "region" : "USA"
+                } 
+            }
+        ]
+    },    
+        
+        
+    {
+        //key : value
+        "name" : "Avatar",
+        "genre" : "Sci-Fi",
+        "reviews" : [
+            {
+                "rating" : 5,
+                "like" : true,
+                "user" : {
+                    "name" : "Jacob Schafer",
+                    "gender" : "male",
+                    "region" : "USA"
+                }
+            },
+            {
+                "rating" : 1,
+                "like" : false,
+                "user" : {
+                    "name" : "Cody Schafer",
+                    "gender" : "male",
+                    "region" : "USA"
+                }
+            }
+        ]
+    }
+     
+        
+    ]
+    
+    
+    return{
+        "movies" : movies
+    } 
+    
+}
